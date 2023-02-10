26f49febb3f5d5a7404fd4bc0d095860fe379e68
htetzawphyo <htetzawphyo.hpn@gmail.com>
2023-02-10 20:48:04 +0630

    ums

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\UsersController::class, 'index'])->name('home');
