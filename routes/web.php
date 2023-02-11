43fde8bed2fe9298a24ab0363b288bafad53060f
htetzawphyo <htetzawphyo.hpn@gmail.com>
2023-02-11 10:51:18 +0630

    check auth

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RolesController;
 use App\Http\Controllers\UsersController;
 
-Route::controller(RolesController::class)->group(function() {
-    Route::get('/roles', 'index')->name('roles');
-    Route::post('/roles/store', 'store')->name('roles.store');
-    Route::get('/roles/{role}', 'edit')->name('roles.edit');
-    Route::put('/roles/{role}', 'update')->name('roles.update');
-    Route::delete('/roles/{role}', 'delete')->name('roles.delete');
-});
-
-Route::controller(UsersController::class)->group(function() {
-    Route::get('/users', 'index')->name('users');
-    Route::post('/users/store', 'store')->name('users.store');
-    Route::get('/users/add', 'add')->name('users.add');
-    Route::get('/users/{user}', 'edit')->name('users.edit');
-    Route::put('/users/{user}', 'update')->name('users.update');
-    Route::delete('/users/{user}', 'delete')->name('users.delete');
+Route::group(['middleware' => 'auth'], function() {
+    Route::controller(RolesController::class)->group(function() {
+        Route::get('/roles', 'index')->name('roles');
+        Route::post('/roles/store', 'store')->name('roles.store');
+        Route::get('/roles/{role}', 'edit')->name('roles.edit');
+        Route::put('/roles/{role}', 'update')->name('roles.update');
+        Route::delete('/roles/{role}', 'delete')->name('roles.delete');
+    });
+    
+    Route::controller(UsersController::class)->group(function() {
+        Route::get('/users', 'index')->name('users');
+        Route::post('/users/store', 'store')->name('users.store');
+        Route::get('/users/add', 'add')->name('users.add');
+        Route::get('/users/{user}', 'edit')->name('users.edit');
+        Route::put('/users/{user}', 'update')->name('users.update');
+        Route::delete('/users/{user}', 'delete')->name('users.delete');
+    });
+    Route::get('/home', [App\Http\Controllers\UsersController::class, 'index'])->name('home');
 });
 
 Route::get('/', function () {    
@@ -28,4 +31,3 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\UsersController::class, 'index'])->name('home');
